29c91d5b4ff95462bc0a661f70e83518d23d2dc0
Vartikasinghgautam <107786347+Vartikasinghgautam@users.noreply.github.com>
2022-06-18 23:50:48 -0700

    doing first commit , uploading basic program

---
+++ addition.java
@@ -0,0 +1,12 @@
+class addition {
+    public static void main(String[] args)
+    {
+        int a=0 , b = 0 ;
+        System.out.println("Enter the number1: ", a);
+        System.out.println("Enter the second number2: ",b);
+        a + b = b;
+        b - a = a;
+        System.out.println("Print a, b"); 
+
+    }
+}
